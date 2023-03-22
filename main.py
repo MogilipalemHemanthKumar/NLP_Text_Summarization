58c8f8501ce48f042ade206528102f7286324707
MogilipalemHemanthKumar <107172150+MogilipalemHemanthKumar@users.noreply.github.com>
2023-03-22 17:55:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+def sumy_summarizer(docx):
+    parser=PlaintextParser.from_string(docx,Tokenizer("english"))
+    lex_summarizer=LexRankSummarizer()
+    summary=lex_summarizer(parser.document,3)
+    summary_list=[str(sentence) for sentence in summary]
+    result= " ".join(summary_list)
+    return result
+
+def main():
+    st.title("Text Summarization")
+    input_text = st.text_area("Enter text here:")
+
+    if st.button("Summarize"):
+
+       summary = sumy_summarizer(input_text)
+       st.success(summary)
+if __name__ == "__main__":
+    main()
